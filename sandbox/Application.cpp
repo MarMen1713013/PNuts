ce1c8a151b8b2487c397a67bd091dfec5150cba8
Marco <marcomenchetti96@gmail.com>
2023-05-14 18:39:50 +0200

    Testing in sadbox: to be removed

--- sandbox/Application.cpp
+++ sandbox/Application.cpp
@@ -1,5 +1,9 @@
+#include "pnuts_engine/events/application_event.hpp"
+#include "pnuts_engine/events/event.hpp"
+#include "pnuts_engine/events/mouse_event.hpp"
 #include <pnuts_engine/IApplication.hpp>
 #include <pnuts_engine/log.hpp>
+#include <pnuts_engine/pnuts_engine.h>
 
 class Application : public PNuts::IApplication{
 public:
@@ -10,6 +14,13 @@ public:
     virtual ~Application() {
     }
     void run() override {
+        PNuts::MouseMovedEvent e(12.0,12.0);
+        if(e.is_in_category(PNuts::event_category_application)) {
+            PNUTS_TRACE(e.to_string());
+        } else {
+            PNUTS_ERROR("Wrong event class!");
+            PNUTS_ERROR(e.to_string());
+        }
         while(true) {}
     }
 };
